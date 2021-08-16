f08ba2406e3812289035395512a53d8c38d60165
Thuan Le <devicedev4@biotricity.com>
2021-08-17 00:13:42 +0700

    update: Tx power

--- example/ble_peripheral/simpleBlePeripheral/main.c
+++ example/ble_peripheral/simpleBlePeripheral/main.c
@@ -147,7 +147,7 @@ static void ble_mem_init_config(void)
 static void hal_rfphy_init(void)
 {
   //============config the txPower
-  g_rfPhyTxPower = RF_PHY_TX_POWER_0DBM;
+  g_rfPhyTxPower = RF_PHY_TX_POWER_5DBM;
   //============config BLE_PHY TYPE
   g_rfPhyPktFmt = PKT_FMT_BLE1M;
   //============config RF Frequency Offset
